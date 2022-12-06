5259a48dc540f4c543bd6227f97c9ab075f28f5d
Thomas Crha <thomas@9bitbyte.com>
2022-12-06 16:34:24 +1100

    day 6 part 1 finished

--- day6/day6-1.py
+++ day6/day6-1.py
@@ -10,4 +10,22 @@ def get_input():
     with open("./day6.input") as packet:
         return packet.read().strip()
 
-print(get_input())
+def get_marker(packet):
+    marker_set = list()
+    for inx, character in enumerate(packet):
+
+        if inx in [0, 1, 2]:
+            marker_set.append(character)
+            continue
+
+        marker_set.append(character)
+        # check if all chars are unique
+        if len(marker_set) == len(set(marker_set)):
+            return inx
+
+        if len(marker_set) > 4:
+            # remove the first item
+            marker_set = marker_set[1:]
+
+print(get_marker(get_input()))
+
